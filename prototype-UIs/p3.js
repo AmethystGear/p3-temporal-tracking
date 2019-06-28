7d187691834ea197705fea1be125d1622b2a61ea
Shiven Bhatt <shivenbhatt@outlook.com>
2019-06-28 11:23:07 -0700

    testing image removal code

--- prototype-UIs/p3.js
+++ prototype-UIs/p3.js
@@ -2,11 +2,13 @@ $(document).ready(function(){
     for(let i = 2020; i > 2000; i--){
         let source = "temporal-tracking-photos\\" + i + ".png";
         let html = "<img src=" + source + "onError='deleteSelf(this);'/>"
+
         $("#body").append(html);
         console.log(source);
     }
 });
 
 function deleteSelf(_this){
+    console.log("removed myself");
     ($_this).remove();
 }
