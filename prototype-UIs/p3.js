8cb6d5320ad91fcd69b99a6f4c7ad0453c11e871
Shiven Bhatt <shivenbhatt@outlook.com>
2019-06-28 13:47:35 -0700

    modified js to correctly crop

--- prototype-UIs/p3.js
+++ prototype-UIs/p3.js
@@ -1,6 +1,5 @@
 let currentImageId = "";
-let imageDict = {};
-let ctx;
+let currentImage;
 
 $(document).ready(function(){
     for(let i = 2020; i > 2000; i--){
@@ -9,17 +8,21 @@ $(document).ready(function(){
             let id = i + (i1 % 2 == 1 ? "b" : "");    
             currentImageId = id;
             let source = "..\\temporal-tracking-photos\\" + id + ".png";
-            let html = "<img style = 'max-width: 100%; max-height: 200px; overflow: hidden;' id = '" + id + "' src= '" + source + "' onerror='deleteSelf(this)' onmousemove='zoomHandler(event)'/>"        
+            let html = "<img style = 'max-width: 100%; max-height: 200px; overflow: hidden;' id = '" + id + "' src= '" + source + "' onerror='deleteSelf(this)' onmousemove='zoomHandler(event)' onmouseover = 'setImg(this)'/>"        
             $("#body").append(html);
         }
     }
     ctx = document.getElementById("zoomArea").getContext("2d");
 });
 
+function setImg(this){
+    currentImage = this;
+}
+
 function zoomHandler(e){    
-    let factorX =  e.target.naturalWidth / e.target.width;
-    let factorY = e.target.naturalHeight/ e.target.height;
-    ctx.drawImage(e.target, e.clientX * factorX, e.clientY * factorY, 100, 100, 0, 0, 300, 300);
+    let factorX =  currentImage.naturalWidth / currentImage.width;
+    let factorY = currentImage.naturalHeight/ currentImage.height;
+    ctx.drawImage(currentImage, e.clientX * factorX, e.clientY * factorY, 100, 100, 0, 0, 300, 300);
 }
 
 function deleteSelf(_this){
