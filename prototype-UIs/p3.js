7612b7e8ed9ed09f9fc7f7aeaab714d676dae4d1
Shiven Bhatt <shivenbhatt@outlook.com>
2019-06-28 11:45:48 -0700

    refactored image id code

--- prototype-UIs/p3.js
+++ prototype-UIs/p3.js
@@ -1,9 +1,10 @@
 let currentImageId = "";
 $(document).ready(function(){
     for(let i = 2020; i > 2000; i--){
-        for(let i1 = 0; i1 < 2; i1++){
+        for(let i1 = 0; i1 < 2; i1++){            
             //check for the b as well.           
-            let id = i + (i1 % 2 == 1 ? "b" : "");       
+            let id = i + (i1 % 2 == 1 ? "b" : "");    
+            currentImageId = id;   
             let source = "..\\temporal-tracking-photos\\" + id + ".png";
             let html = "<img id = '" + id + "' src= '" + source + "' onError='deleteSelf()'/>"        
             $("#body").append(html);
