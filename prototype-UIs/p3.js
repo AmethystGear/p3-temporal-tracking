3a842766a3a26701abc73c69183b802b03bebe88
Shiven Bhatt <shivenbhatt@outlook.com>
2019-06-28 12:44:37 -0700

    adding zoom canvas

--- prototype-UIs/p3.js
+++ prototype-UIs/p3.js
@@ -1,16 +1,25 @@
 let currentImageId = "";
+let imageDict = {};
+
+
 $(document).ready(function(){
     for(let i = 2020; i > 2000; i--){
         for(let i1 = 0; i1 < 2; i1++){            
             //check for the b as well.           
             let id = i + (i1 % 2 == 1 ? "b" : "");    
-            currentImageId = id;   
+            currentImageId = id;
             let source = "..\\temporal-tracking-photos\\" + id + ".png";
-            let html = "<img class = 'gsv' style = 'max-width: 100%' id = '" + id + "' src= '" + source + "' onError='deleteSelf(this)'/>"        
+            let html = "<img class = 'gsv' style = 'max-width: 100%; max-height: 200px; overflow: hidden;' id = '" + id + "' src= '" + source + "' onError='deleteSelf(this)' onmousemove='zoomHandler(event)'/>"        
             $("#body").append(html);
         }
     }
 });
+
+function zoomHandler(e){
+    let ctx = document.getElementById("zoomArea").getContext("2d");
+    ctx.drawImage(e.srcElement, e.clientX, e.clientY, 100, 100, 0, 0, e.srcElement.width, e.srcElement.height);
+}
+
 
 function deleteSelf(_this){
     _this.remove();
