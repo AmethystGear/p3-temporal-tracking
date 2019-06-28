fad51f6c48c77b956a9afe9a2fa5cc6c93f596e9
Shiven Bhatt <shivenbhatt@outlook.com>
2019-06-28 11:17:07 -0700

    testing image creation code

--- prototype-UIs/p3.js
+++ prototype-UIs/p3.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
-    for(let i = 2000; i < 2020; i++){
+    for(let i = 2020; i > 2000; i--){
         let source = "temporal-tracking-photos\\" + i + ".png";
+        let html = "<img src=" + source + "onError='deleteSelf(this);'/>"
         console.log(source);
     }
 });
+
+function deleteSelf(_this){
+    ($_this).remove();
+}
