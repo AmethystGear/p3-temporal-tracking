11bb379b46a40a24e38ade4a6dc3a7c26faad4da
Shiven Bhatt <shivenbhatt@outlook.com>
2019-06-28 11:31:24 -0700

    fixing filepath issues

--- prototype-UIs/p3.js
+++ prototype-UIs/p3.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     for(let i = 2020; i > 2000; i--){
         let source = "..\\temporal-tracking-photos\\" + i + ".png";
-        let html = "<img src=" + source + "onError='deleteSelf(this);'/>"
+        let html = "<img src= '" + source + "' onError='deleteSelf(this);'/>"
 
         $("#body").append(html);
         console.log(source);
