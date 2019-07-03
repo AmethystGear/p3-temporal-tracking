7c0fa78e2221fee7314b89a2a3ceae1f926fd251
AmethystGear <shivenbhatt@outlook.com>
2019-07-03 10:25:17 -0700

    generalized p3a.js to work for a and b, renamed to p3.js

--- prototype-UIs/p3.js
+++ prototype-UIs/p3.js
@@ -1,16 +1,21 @@
-let currentImageId = "";
-let zoomCtx;
-let zoomCanvas;
-let boundingBoxes = [];
 $(document).ready(function(){
-    for(let i = 2020; i > 2000; i--){
+    var dateRange = $('#date-range').attr('search').split("-");
+    var firstBound = parseInt(dateRange[0]);
+    var secondBound =  parseInt(dateRange[1]);
+    var diff = (secondBound - firstBound) / Math.abs(secondBound - firstBound);
+
+    var typeAndNum = $('#date-range').attr('get').split(':');
+    var type = typeAndNum[0];
+    var num = parseInt(typeAndNum[1]);    
+
+    for(let i = firstBound; i != secondBound; i += diff){
         for(let i1 = 0; i1 < 2; i1++){            
             //check for the b as well.           
             let id = i + (i1 % 2 == 1 ? "b" : "");    
             currentImageId = id;
             let source = "..\\temporal-tracking-photos\\" + id + ".png";
             let image = 
-                    "<div style = 'max-width: 100%; height: 220px; position:relative;' onmousemove='zoomHandler(event)'>" + 
+                    "<div class = 'canvas-image-container' style = 'max-width: 100%; position:relative;' onmousemove='mouseMove(event)'>" + 
                         "<img class = 'coveredImage'  id = '" + id + "' src= '" + source + "' onerror='deleteSelf(this)'/>" +
                         "<canvas class = 'coveringCanvas' id = '" + id + "canvas'></canvas>" +
                     "</div>"
@@ -18,118 +23,33 @@ $(document).ready(function(){
             $("#body").append(image);
         }
     }
-    zoomCanvas = document.getElementById("zoomArea");
-    zoomCtx = zoomCanvas.getContext("2d");
-    zoomCtx.fillStyle = "yellow";    
+
+    // Handle the deltion of the photos after the invalid photos get deleted.
+    setTimeout(100, handlePhotos(type, num));
 });
 
-
-var xClick = -1;
-var yClick = -1;
-var mouseDown = 0;
-document.body.onmousedown = function() { 
-  ++mouseDown;
-}
-document.body.onmouseup = function() {
-  --mouseDown;
-  xClick = -1;
-  yClick = -1;
-  console.log(boundingBox);
-  console.log(boundingBoxes);
-  boundingBoxes.push(boundingBox);
-}
-
-var zoomXWidth = 100;
-var zoomYWidth = 100;
-var zoomImage;
-var canvasImage;
-var boundingBox = null;
-var xZoomCropClick;
-var yZoomCropClick;
-
-function zoomHandler(e){  
-
-    var canvas = e.target;
-    var image = document.getElementById(canvas.id.replace("canvas", ""));
-    //getting the x,y of the mouse  
-    let factorX =  image.naturalWidth / image.width;
-    let factorY = image.naturalHeight/ image.height;
-    var rect = canvas.getBoundingClientRect();
-    var x = e.clientX - rect.left;
-    var y = e.clientY - rect.top;
-
-    //drawing visual indicator of zoom
-    let canvasCtx = canvas.getContext("2d");
-    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-    canvasCtx.globalAlpha = 0.2;
-
-    var zoomXWidthCalculated = zoomXWidth/image.width * canvas.width;
-    var zoomYWidthCalculated = zoomYWidth/200 * canvas.height;
-    var xCalculated = Math.clamp(x/image.width * canvas.width, zoomXWidthCalculated/2, canvas.width - zoomXWidthCalculated/2);
-    var yCalculated = Math.clamp(y/image.height * canvas.height, zoomYWidthCalculated/2, image.height - zoomYWidthCalculated);
-
-    
-    var xZoomCrop = (xCalculated * image.width/canvas.width - zoomXWidth/2)* factorX;
-    var yZoomCrop = (yCalculated * 200/canvas.height - zoomYWidth/2) * factorY;
-    if(mouseDown){
-        if(xClick === -1){
-            xClick = x;
-            yClick = y;
-            zoomImage = zoomCtx.getImageData(0,0,zoomCanvas.width, zoomCanvas.height);
-            xZoomCropClick = xZoomCrop;
-            yZoomCropClick = yZoomCrop;
-        }
-        canvasCtx.fillStyle = "yellow";
-        var diffX = x - xClick;
-        var diffY = y - yClick;
-
-
-        canvasCtx.fillRect(xClick/image.width * canvas.width, yClick/200 * canvas.height, diffX/image.width * canvas.width, diffY/200 * canvas.height);
-        zoomCtx.clearRect(0,0, zoomCanvas.width, zoomCanvas.height);
-        zoomCtx.globalAlpha = 1.0;
-        zoomCtx.putImageData(zoomImage, 0, 0);
-        zoomCtx.globalAlpha = 0.2;
-        zoomCtx.fillStyle = "yellow";
-        let box = {x: xClick, y: yClick, width: diffX, height: diffY};
-        zoomCtx.fillRect((box.x - (xZoomCropClick/factorX)) / zoomXWidth * zoomCanvas.width, (box.y - (yZoomCropClick/factorY)) /zoomYWidth* zoomCanvas.height, (diffX) / zoomXWidth * zoomCanvas.width, (diffY) /zoomYWidth* zoomCanvas.height);
-        zoomCtx.globalAlpha = 1.0;
-        zoomCtx.fillStyle = "black";
-        zoomCtx.fillRect(-5 + (x - (xZoomCropClick/factorX)) / zoomXWidth * zoomCanvas.width, -5 + (y - (yZoomCropClick/factorY)) /zoomYWidth * zoomCanvas.height, 10, 10);
-
-        zoomCtx.fillStyle = "white";        
-        zoomCtx.fillRect(-3 + (x - (xZoomCropClick/factorX)) / zoomXWidth * zoomCanvas.width, -3 + (y - (yZoomCropClick/factorY)) /zoomYWidth * zoomCanvas.height, 6, 6);
-        
-        boundingBox = box;
+// Enables us to only display the first or last 'num' photos, or display all photos. 
+function handlePhotos(type, num){
+    if(type === "all"){
         return;
     }
-
-
-
-    zoomCtx.globalAlpha = 1.0;
-    //drawing zoomed image at the top
-    zoomCtx.drawImage(image, xZoomCrop, yZoomCrop, zoomXWidth * factorX, zoomYWidth * factorY, 0, 0, zoomCanvas.width, zoomCanvas.height);
-    
-    zoomCtx.fillStyle = "black";
-    zoomCtx.fillRect(-5 + (x - (xZoomCrop/factorX)) / zoomXWidth * zoomCanvas.width, -5 + (y - (yZoomCrop/factorY)) /zoomYWidth * zoomCanvas.height, 10, 10);
-
-    zoomCtx.fillStyle = "white";        
-    zoomCtx.fillRect(-3 + (x - (xZoomCrop/factorX)) / zoomXWidth * zoomCanvas.width, -3 + (y - (yZoomCrop/factorY)) /zoomYWidth * zoomCanvas.height, 6, 6);
-   
-    zoomCtx.globalAlpha = 0.2;
-    canvasCtx.fillStyle = "yellow";
-    zoomCtx.fillStyle = "yellow";
-    for(var i = 0; i < boundingBoxes.length; i++){
-        var box = boundingBoxes[i];
-        canvasCtx.fillRect(box.x/image.width * canvas.width, box.y/image.height * canvas.height, box.width/image.width * canvas.width, box.height/image.height * canvas.height);
-        zoomCtx.fillRect((box.x - (xZoomCrop/factorX)) / zoomXWidth * zoomCanvas.width, (box.y - (yZoomCrop/factorY)) /zoomYWidth* zoomCanvas.height, (box.width) / zoomXWidth* zoomCanvas.width, (box.height) /zoomYWidth * zoomCanvas.height);
+    var numCanvasImageContainers = $('.canvas-image-container').length;
+    if(type === "first"){
+        $('.canvas-image-container').each(function(i, obj){
+            if(i >= num){
+                obj.remove();
+            }
+        });
     }
-
-    canvasCtx.fillStyle = "blue";
-    canvasCtx.fillRect(xCalculated - zoomXWidthCalculated/2, yCalculated - zoomYWidthCalculated/2, zoomXWidthCalculated, zoomYWidthCalculated);   
+    if(type === "last"){
+        $('.canvas-image-container').each(function(i, obj){
+            if(i < numCanvasImageContainers - num){
+                obj.remove();
+            }
+        });
+    }
 }
 
 function deleteSelf(_this){
     _this.parentNode.remove();
 }
-
-(function(){Math.clamp=function(a,b,c){return Math.max(b,Math.min(c,a));}})();
---
+++ prototype-UIs/zoomAndBoundingBox.js
@@ -0,0 +1,118 @@
+let currentImageId = "";
+let boundingBoxes = [];
+$(document).ready(function(){
+    zoomCanvas = document.getElementById("zoomArea");
+    zoomCtx = zoomCanvas.getContext("2d");
+});
+
+let zoomCtx;
+let zoomCanvas;
+var xClick = -1;
+var yClick = -1;
+var mouseDown = 0;
+document.body.onmousedown = function() { 
+  ++mouseDown;
+}
+document.body.onmouseup = function() {
+  --mouseDown;
+  xClick = -1;
+  yClick = -1;
+  boundingBoxes.push(boundingBox);
+  zoomHandler(mostRecentMouseEvent);
+}
+
+var zoomXWidth = 100;
+var zoomYWidth = 100;
+var zoomImage;
+var canvasImage;
+var mostRecentMouseEvent;
+var boundingBox = null;
+var xZoomCropClick;
+var yZoomCropClick;
+
+function mouseMove(e){
+    mostRecentMouseEvent = e;
+    zoomHandler(e);
+}
+
+function zoomHandler(e){
+    var canvas = e.target;
+    var image = document.getElementById(canvas.id.replace("canvas", ""));
+    //getting the x,y of the mouse  
+    let factorX =  image.naturalWidth / image.width;
+    let factorY = image.naturalHeight/ image.height;
+    var rect = canvas.getBoundingClientRect();
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+
+    //drawing visual indicator of zoom
+    let canvasCtx = canvas.getContext("2d");
+    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+    canvasCtx.globalAlpha = 0.2;
+
+    var zoomXWidthCalculated = zoomXWidth/image.width * canvas.width;
+    var zoomYWidthCalculated = zoomYWidth/200 * canvas.height;
+    var xCalculated = Math.clamp(x/image.width * canvas.width, zoomXWidthCalculated/2, canvas.width - zoomXWidthCalculated/2);
+    var yCalculated = Math.clamp(y/image.height * canvas.height, zoomYWidthCalculated/2, image.height - zoomYWidthCalculated);
+
+    
+    var xZoomCrop = (xCalculated * image.width/canvas.width - zoomXWidth/2)* factorX;
+    var yZoomCrop = (yCalculated * 200/canvas.height - zoomYWidth/2) * factorY;
+    if(mouseDown){
+        if(xClick === -1){
+            xClick = x;
+            yClick = y;
+            zoomImage = zoomCtx.getImageData(0,0,zoomCanvas.width, zoomCanvas.height);
+            xZoomCropClick = xZoomCrop;
+            yZoomCropClick = yZoomCrop;
+        }
+        canvasCtx.fillStyle = "yellow";
+        var diffX = x - xClick;
+        var diffY = y - yClick;
+
+
+        canvasCtx.fillRect(xClick/image.width * canvas.width, yClick/200 * canvas.height, diffX/image.width * canvas.width, diffY/200 * canvas.height);
+        zoomCtx.clearRect(0,0, zoomCanvas.width, zoomCanvas.height);
+        zoomCtx.globalAlpha = 1.0;
+        zoomCtx.putImageData(zoomImage, 0, 0);
+        zoomCtx.globalAlpha = 0.2;
+        zoomCtx.fillStyle = "yellow";
+        let box = {x: xClick, y: yClick, width: diffX, height: diffY};
+        zoomCtx.fillRect((box.x - (xZoomCropClick/factorX)) / zoomXWidth * zoomCanvas.width, (box.y - (yZoomCropClick/factorY)) /zoomYWidth* zoomCanvas.height, (diffX) / zoomXWidth * zoomCanvas.width, (diffY) /zoomYWidth* zoomCanvas.height);
+        zoomCtx.globalAlpha = 1.0;
+        zoomCtx.fillStyle = "black";
+        zoomCtx.fillRect(-5 + (x - (xZoomCropClick/factorX)) / zoomXWidth * zoomCanvas.width, -5 + (y - (yZoomCropClick/factorY)) /zoomYWidth * zoomCanvas.height, 10, 10);
+
+        zoomCtx.fillStyle = "white";        
+        zoomCtx.fillRect(-3 + (x - (xZoomCropClick/factorX)) / zoomXWidth * zoomCanvas.width, -3 + (y - (yZoomCropClick/factorY)) /zoomYWidth * zoomCanvas.height, 6, 6);
+        
+        boundingBox = box;
+        return;
+    }
+
+
+
+    zoomCtx.globalAlpha = 1.0;
+    //drawing zoomed image at the top
+    zoomCtx.drawImage(image, xZoomCrop, yZoomCrop, zoomXWidth * factorX, zoomYWidth * factorY, 0, 0, zoomCanvas.width, zoomCanvas.height);
+    
+    zoomCtx.fillStyle = "black";
+    zoomCtx.fillRect(-5 + (x - (xZoomCrop/factorX)) / zoomXWidth * zoomCanvas.width, -5 + (y - (yZoomCrop/factorY)) /zoomYWidth * zoomCanvas.height, 10, 10);
+
+    zoomCtx.fillStyle = "white";        
+    zoomCtx.fillRect(-3 + (x - (xZoomCrop/factorX)) / zoomXWidth * zoomCanvas.width, -3 + (y - (yZoomCrop/factorY)) /zoomYWidth * zoomCanvas.height, 6, 6);
+   
+    zoomCtx.globalAlpha = 0.2;
+    canvasCtx.fillStyle = "yellow";
+    zoomCtx.fillStyle = "yellow";
+    for(var i = 0; i < boundingBoxes.length; i++){
+        var box = boundingBoxes[i];
+        canvasCtx.fillRect(box.x/image.width * canvas.width, box.y/image.height * canvas.height, box.width/image.width * canvas.width, box.height/image.height * canvas.height);
+        zoomCtx.fillRect((box.x - (xZoomCrop/factorX)) / zoomXWidth * zoomCanvas.width, (box.y - (yZoomCrop/factorY)) /zoomYWidth* zoomCanvas.height, (box.width) / zoomXWidth* zoomCanvas.width, (box.height) /zoomYWidth * zoomCanvas.height);
+    }
+
+    canvasCtx.fillStyle = "blue";
+    canvasCtx.fillRect(xCalculated - zoomXWidthCalculated/2, yCalculated - zoomYWidthCalculated/2, zoomXWidthCalculated, zoomYWidthCalculated);   
+}
+
+(function(){Math.clamp=function(a,b,c){return Math.max(b,Math.min(c,a));}})();
